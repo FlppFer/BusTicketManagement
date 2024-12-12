546b4e41f3deb67d85ac9abba81745009fc44773
Fellippo Ferreira <fellippodias@gmail.com>
2024-12-11 22:37:35 -0300

    first commit

---
+++ src/main/java/com/FellippoFerreira/BusTicketsManagement/BusTicketsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.FellippoFerreira.BusTicketsManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FellippoFerreira/BusTicketsManagement/BusTicketsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FellippoFerreira.BusTicketsManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
